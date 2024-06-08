05b5f3dbdc8281946a5a880f46c65c508f809495
doctorbond2 <michael.rostlund@hotmail.com>
2024-06-08 20:13:16 +0200

    chore: Update Flask configuration and dependencies for server

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,4 +1,14 @@
-# from flask import Flask
-# from flask_cors import CORS
-# app = Flask(__name__)
-# CORS(app) 
+from flask import Flask
+from flask_cors import CORS
+from dotenv import load_dotenv
+from flask_cors import CORS
+import os
+
+  
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app) 
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    return app
--- server/server.py
+++ server/server.py
@@ -1,16 +1,20 @@
 from flask import Flask, redirect
-from flask_cors import CORS
-app = Flask(__name__)
-CORS(app) 
-print("Running main.py")
+from app import create_app
+from flask_sqlalchemy import SQLAlchemy
+app = create_app()
+db = SQLAlchemy(app) 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!<p/>"
-
 @app.route("/test", methods=['GET'])
 def test():
     print("test")
-    return "<p>Test<p/> "
+    return redirect("/")
 # make visible server = flask --app main run --host=0.0.0.0
 
 if __name__ == "__main__":
