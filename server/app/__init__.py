7f13a188ff6ac12aec98042580c0cc5a588c4e9c
doctorbond2 <michael.rostlund@hotmail.com>
2024-06-12 13:17:10 +0200

    chore: Update Flask configuration and dependencies, add SQLAlchemy for database management, and create user controller and model

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -7,12 +7,12 @@ from app.db import db
 import os
 
 
-
 def create_app():
     app = Flask(__name__)
     CORS(app) 
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['']
     app.register_blueprint(blueprints.game_bp)
     db.__init__(app)
     return app
