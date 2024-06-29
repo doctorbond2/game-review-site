8aa1e0bdd393d29e54ca33f8c16d1f91d69f9d83
doctorbond2 <michael.rostlund@hotmail.com>
2024-06-29 12:24:53 +0200

    chore: Remove commented-out code and unused function in game controller

--- server/app/controllers/game.py
+++ server/app/controllers/game.py
@@ -10,21 +10,11 @@ def index():
     # try:
     #     game_list = session.query(Game).options(joinedload(Game.systems)).all()
     #     print (game_list)
-    #     return jsonify([game.to_dict(session) for game in game_list])
+    #     return jsonify([game.to_dict() for game in game_list])
     # except Exception as e:
     #     print(f'Error: {e}')
     #     session.rollback()
     #     return jsonify({'message': 'An error occured'}), 500
-    session = db.session()
-    try:
-        genre_list = session.query(Genre).options(joinedload(Genre.games)).all()
-        return jsonify([genre.to_dict() for genre in genre_list])
-    except Exception as e:
-        print(f'Error: {e}')
-        session.rollback()
-        return jsonify({'message': 'An error occured'}), 500
-   
-def index_two():
     session = db.session()
     try:
         genre_list = session.query(Genre).all()
@@ -33,4 +23,14 @@ def index_two():
         print(f'Error: {e}')
         session.rollback()
         return jsonify({'message': 'An error occured'}), 500
+   
+# def index_two():
+#     session = db.session()
+#     try:
+#         genre_list = session.query(Genre).all()
+#         return jsonify([genre.to_dict() for genre in genre_list])
+#     except Exception as e:
+#         print(f'Error: {e}')
+#         session.rollback()
+#         return jsonify({'message': 'An error occured'}), 500
 
