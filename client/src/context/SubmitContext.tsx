26682491fbd3e5ec76995793b338d83c76ecd899
doctorbond2 <michael.rostlund@hotmail.com>
2024-08-12 22:20:43 +0200

    feat: Refactor SubmitContext to handle login data
    The code changes in SubmitContext.tsx refactor the submit function to handle login data. The function now checks if the type is 'login' and updates the data object accordingly, either using the email or username property. This improvement enhances the functionality of the SubmitContext by properly handling login data.
    
    UPDATED: SubmitContext.tsx

--- client/src/context/SubmitContext.tsx
+++ client/src/context/SubmitContext.tsx
@@ -3,6 +3,8 @@ import React, { createContext, useContext, useState } from 'react';
 import { TYPE_SubmitContext as SC } from '@/types/types_submit';
 import { defaultSubmitContext } from '@/types/default_submit';
 import { isEmail } from '@/utils/clientFunctions';
+import { log } from 'console';
+import { TYPE_loginData } from '@/types/types_submit';
 type Props = { children: React.ReactNode };
 
 const SubmitContext = createContext<SC>(defaultSubmitContext);
@@ -20,19 +22,20 @@ function SubmitProvider({ children }: Props) {
 
   const resetSubmit = () => setSubmitData({});
 
-  const submit = (data: Record<string, any>, type: string) => {
+  const submit = (data: TYPE_loginData, type: string) => {
     if (type === 'login') {
-      if (isEmail(data['user-login-username-or-email'])) {
+      if (isEmail(data['user-login-username-or-email'] as string)) {
         data['email'] = data['user-login-username-or-email'];
         delete data['user-login-username-or-email'];
         console.log('Email');
       } else {
         data['username'] = data['user-login-username-or-email'];
-        delete data['user-login-username-or-email']; // Remove the old key if necessary
+        delete data['user-login-username-or-email'];
         console.log('Username');
       }
     }
   };
+
   return (
     <SubmitContext.Provider
       value={{ submitData, setSubmitData, resetSubmit, submit }}
--- client/src/types/types_submit.ts
+++ client/src/types/types_submit.ts
@@ -4,3 +4,9 @@ export type TYPE_SubmitContext = {
   resetSubmit: () => void;
   submit: (data: Record<string, any>, type: string) => void;
 };
+export type TYPE_loginData = {
+  password?: string;
+  'user-login-username-or-email'?: string;
+  email?: string;
+  username?: string;
+};
